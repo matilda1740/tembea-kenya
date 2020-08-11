eaec306efcb367e011ea26f188f661e64da44495
Matilda <matilda1740@gmail.com>
2020-08-11 23:31:24 +0300

    first tour commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import './App.scss';
-import Tours from './components/TourList/Tourslist'
-import Navbar from '../src/components/Navbar/Navbar'
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import Tours from './components/TourList/Tourslist';
+import Navbar from '../src/components/Navbar/Navbar';
 
 function App() {
-  return (
-    <section className="App">
-      <Navbar />
-      <Tours />
-    </section>
-  );
+	return (
+		<section className="App">
+			<Navbar />
+			<Tours />
+		</section>
+	);
 }
 
 export default App;
